b5ff7ac885bb8633a4ee5edf4c09dcea40201af1
Daniel Still <80142739+StillScripts@users.noreply.github.com>
2022-06-19 22:44:44 +1000

    Add header buttons

--- src/App.tsx
+++ src/App.tsx
@@ -21,24 +21,66 @@ const App: Component = () => {
 
   return (
     <div class={styles.container}>
-      <main class={styles.examples}>
-        <Switch fallback={<MatrixExample callback={() => setShowNext(true)} />}>
-          <Match when={example() === "matrix"}>
-            <MatrixExample callback={() => setShowNext(true)} />
-          </Match>
-          <Match when={example() === "rainbow"}>
-            <RainbowExample />
-          </Match>
-        </Switch>
-        <Show when={showNext()}>
-          <Button callback={handleNext} show={showNext()}>
-            Show Next
-          </Button>
-        </Show>
-      </main>
-      <footer class={styles.example}>
+      <div>
+        <header>
+          <button
+            class={styles.link}
+            style={
+              example() === "matrix"
+                ? {
+                    color: "rgba(255, 255, 255, 0.9)",
+                    "text-decoration": "underline",
+                    "text-decoration-color": "#2DD623",
+                  }
+                : {}
+            }
+            onClick={() => setExample("matrix")}
+          >
+            Matrix
+          </button>
+          <button
+            class={styles.link}
+            style={
+              example() === "rainbow"
+                ? {
+                    color: "rgba(255, 255, 255, 0.9)",
+                    "text-decoration": "underline",
+                    "text-decoration-color": "#2DD623",
+                  }
+                : {}
+            }
+            onClick={() => setExample("rainbow")}
+          >
+            Rainbow
+          </button>
+        </header>
+        <main>
+          <Switch
+            fallback={<MatrixExample callback={() => setShowNext(true)} />}
+          >
+            <Match when={example() === "matrix"}>
+              <MatrixExample callback={() => setShowNext(true)} />
+            </Match>
+            <Match when={example() === "rainbow"}>
+              <RainbowExample />
+            </Match>
+          </Switch>
+          <Show when={showNext()}>
+            <div>
+              <Button callback={handleNext} show={showNext()}>
+                Show Next
+              </Button>
+            </div>
+          </Show>
+        </main>
+      </div>
+      <footer>
         <div>Made by Daniel Still</div>
-        <div><a href="https://github.com/StillScripts/solid-typer" target="_blank">Source Code</a></div>
+        <div>
+          <a href="https://github.com/StillScripts/solid-typer" target="_blank">
+            Source Code
+          </a>
+        </div>
       </footer>
     </div>
   );
